898225e59c344ec69bac7ad60f23e53db15aec4c
loking23 <mongrock30102@gmail.comm>
2023-01-14 17:38:03 +0800

    fix: dynamic import 讀不到底線的問題

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,40 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
+// 解決 vite dynamic import 時無法讀取含有下底線的檔案
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/' : '/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src') // 路径别名
-    }
-  },
-  server: {
-      host: '0.0.0.0'
-  },
-  plugins: [vue()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@use "src/assets/scss/index.scss" as *;',
-      },
+    base: process.env.NODE_ENV === 'production' ? '/' : '/',
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'), // 路径别名
+        },
     },
-  },
-})
+    server: {
+        host: '0.0.0.0',
+    },
+    plugins: [vue()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: '@use "src/assets/scss/index.scss" as *;',
+            },
+        },
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+                sanitizeFileName(fileName) {
+                    const match = DRIVE_LETTER_REGEX.exec(fileName);
+                    const driveLetter = match ? match[0] : '';
+                    return driveLetter + fileName.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '');
+                },
+            },
+        },
+    },
+});
